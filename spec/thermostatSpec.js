71469b9f96f40bdd80f5e5b247df2b6803da9371
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2017-02-22 12:56:08 +0000

    Guard condition of minimum temp.

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,14 +12,21 @@ describe('Thermostat', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it('increases the tempterature with up()', function(){
+  it('increases the tempterature with up()', function() {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
-  it('decreases the tempterature with up()', function(){
+  it('decreases the tempterature with up()', function() {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum tempterature of 10', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
 
 });
