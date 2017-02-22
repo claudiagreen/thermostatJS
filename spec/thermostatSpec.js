c667e44dc350daafd701ceab2d882e88fce1f4d6
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2017-02-22 12:49:31 +0000

    Up & Down functions added.

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,6 +9,17 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it('increases the tempterature with up()', function(){
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases the tempterature with up()', function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
 });
